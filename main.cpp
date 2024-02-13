4c989d83159165535bd74d027f7db6919abc8139
matgomiero <matgomiero@gmail.com>
2024-02-13 19:59:14 +0100

    Added Assimp 5.2.5

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@
 
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 int main() {
     glfwInit();
 
